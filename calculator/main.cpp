f449f9fe196a19704f865412727e58001b53de19
Batuhan Odçıkın <116088684+batoddy@users.noreply.github.com>
2025-07-17 13:01:38 +0300

    Add files via upload

---
+++ calculator/main.cpp
@@ -0,0 +1,11 @@
+// calculator/main.cpp
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+    std::cout << "5 + 3 = " << sum << std::endl;
+    while (1);
+    return 0;    
+}
---
+++ hello_world/main.cpp
@@ -0,0 +1,9 @@
+// hello_world/main.cpp
+#include <iostream>
+
+int main() {
+    std::cout << "Merhaba Dünya!" << std::endl;
+
+    while (1);
+    return 0;
+}
